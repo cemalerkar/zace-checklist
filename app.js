8bebcf3bd402acadd4e14abd218cefea0236b323
Cemal Erkar <cemalerkar4747@gmail.com>
2023-12-10 23:29:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,465 @@
+var Ara1 = document.getElementById("ara1");
+
+if (localStorage.getItem("Ara1") == "tamamlandi") {
+  Ara1.checked = true;
+}
+
+Ara1.onclick = function () {
+  if (localStorage.getItem("Ara1") == "tamamlandi") {
+    Ara1.checked = false;
+    localStorage.setItem("Ara1", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara1", "tamamlandi");
+    Ara1.checked = true;
+  }
+};
+var Ara2 = document.getElementById("ara2");
+
+if (localStorage.getItem("Ara2") == "tamamlandi") {
+  Ara2.checked = true;
+}
+
+Ara2.onclick = function () {
+  if (localStorage.getItem("Ara2") == "tamamlandi") {
+    Ara2.checked = false;
+    localStorage.setItem("Ara2", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara2", "tamamlandi");
+    Ara2.checked = true;
+  }
+};
+var Ara3 = document.getElementById("ara3");
+
+if (localStorage.getItem("Ara3") == "tamamlandi") {
+  Ara3.checked = true;
+}
+
+Ara3.onclick = function () {
+  if (localStorage.getItem("Ara3") == "tamamlandi") {
+    Ara3.checked = false;
+    localStorage.setItem("Ara3", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara3", "tamamlandi");
+    Ara3.checked = true;
+  }
+};
+var Ara4 = document.getElementById("ara4");
+
+if (localStorage.getItem("Ara4") == "tamamlandi") {
+  Ara4.checked = true;
+}
+
+Ara4.onclick = function () {
+  if (localStorage.getItem("Ara4") == "tamamlandi") {
+    Ara4.checked = false;
+    localStorage.setItem("Ara4", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara4", "tamamlandi");
+    Ara4.checked = true;
+  }
+};
+var Ara5 = document.getElementById("ara5");
+
+if (localStorage.getItem("Ara5") == "tamamlandi") {
+  Ara5.checked = true;
+}
+
+Ara5.onclick = function () {
+  if (localStorage.getItem("Ara5") == "tamamlandi") {
+    Ara5.checked = false;
+    localStorage.setItem("Ara5", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara5", "tamamlandi");
+    Ara5.checked = true;
+  }
+};
+var Ara6 = document.getElementById("ara6");
+
+if (localStorage.getItem("Ara6") == "tamamlandi") {
+  Ara6.checked = true;
+}
+
+Ara6.onclick = function () {
+  if (localStorage.getItem("Ara6") == "tamamlandi") {
+    Ara6.checked = false;
+    localStorage.setItem("Ara6", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara6", "tamamlandi");
+    Ara6.checked = true;
+  }
+};
+var Ara7 = document.getElementById("ara7");
+
+if (localStorage.getItem("Ara7") == "tamamlandi") {
+  Ara7.checked = true;
+}
+
+Ara7.onclick = function () {
+  if (localStorage.getItem("Ara7") == "tamamlandi") {
+    Ara7.checked = false;
+    localStorage.setItem("Ara7", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara7", "tamamlandi");
+    Ara7.checked = true;
+  }
+};
+var Ara8 = document.getElementById("ara8");
+
+if (localStorage.getItem("Ara8") == "tamamlandi") {
+  Ara8.checked = true;
+}
+
+Ara8.onclick = function () {
+  if (localStorage.getItem("Ara8") == "tamamlandi") {
+    Ara8.checked = false;
+    localStorage.setItem("Ara8", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara8", "tamamlandi");
+    Ara8.checked = true;
+  }
+};
+var Ara9 = document.getElementById("ara9");
+
+if (localStorage.getItem("Ara9") == "tamamlandi") {
+  Ara9.checked = true;
+}
+
+Ara9.onclick = function () {
+  if (localStorage.getItem("Ara9") == "tamamlandi") {
+    Ara9.checked = false;
+    localStorage.setItem("Ara9", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara9", "tamamlandi");
+    Ara9.checked = true;
+  }
+};
+var Ara10 = document.getElementById("ara10");
+
+if (localStorage.getItem("Ara10") == "tamamlandi") {
+  Ara10.checked = true;
+}
+
+Ara10.onclick = function () {
+  if (localStorage.getItem("Ara10") == "tamamlandi") {
+    Ara10.checked = false;
+    localStorage.setItem("Ara10", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara10", "tamamlandi");
+    Ara10.checked = true;
+  }
+};
+var Ara11 = document.getElementById("ara11");
+
+if (localStorage.getItem("Ara11") == "tamamlandi") {
+  Ara11.checked = true;
+}
+
+Ara11.onclick = function () {
+  if (localStorage.getItem("Ara11") == "tamamlandi") {
+    Ara11.checked = false;
+    localStorage.setItem("Ara11", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara11", "tamamlandi");
+    Ara11.checked = true;
+  }
+};
+var Ara12 = document.getElementById("ara12");
+
+if (localStorage.getItem("Ara12") == "tamamlandi") {
+  Ara12.checked = true;
+}
+
+Ara12.onclick = function () {
+  if (localStorage.getItem("Ara12") == "tamamlandi") {
+    Ara12.checked = false;
+    localStorage.setItem("Ara12", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara12", "tamamlandi");
+    Ara12.checked = true;
+  }
+};
+var Ara13 = document.getElementById("ara13");
+
+if (localStorage.getItem("Ara13") == "tamamlandi") {
+  Ara13.checked = true;
+}
+
+Ara13.onclick = function () {
+  if (localStorage.getItem("Ara13") == "tamamlandi") {
+    Ara13.checked = false;
+    localStorage.setItem("Ara13", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara13", "tamamlandi");
+    Ara13.checked = true;
+  }
+};
+var Ara14 = document.getElementById("ara14");
+
+if (localStorage.getItem("Ara14") == "tamamlandi") {
+  Ara14.checked = true;
+}
+
+Ara14.onclick = function () {
+  if (localStorage.getItem("Ara14") == "tamamlandi") {
+    Ara14.checked = false;
+    localStorage.setItem("Ara14", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara14", "tamamlandi");
+    Ara14.checked = true;
+  }
+};
+var Ara15 = document.getElementById("ara15");
+
+if (localStorage.getItem("Ara15") == "tamamlandi") {
+  Ara15.checked = true;
+}
+
+Ara15.onclick = function () {
+  if (localStorage.getItem("Ara15") == "tamamlandi") {
+    Ara15.checked = false;
+    localStorage.setItem("Ara15", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara15", "tamamlandi");
+    Ara15.checked = true;
+  }
+};
+var Ara16 = document.getElementById("ara16");
+
+if (localStorage.getItem("Ara16") == "tamamlandi") {
+  Ara16.checked = true;
+}
+
+Ara16.onclick = function () {
+  if (localStorage.getItem("Ara16") == "tamamlandi") {
+    Ara16.checked = false;
+    localStorage.setItem("Ara16", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara16", "tamamlandi");
+    Ara16.checked = true;
+  }
+};
+var Ara17 = document.getElementById("ara17");
+
+if (localStorage.getItem("Ara17") == "tamamlandi") {
+  Ara17.checked = true;
+}
+
+Ara17.onclick = function () {
+  if (localStorage.getItem("Ara17") == "tamamlandi") {
+    Ara17.checked = false;
+    localStorage.setItem("Ara17", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara17", "tamamlandi");
+    Ara17.checked = true;
+  }
+};
+var Ara18 = document.getElementById("ara18");
+
+if (localStorage.getItem("Ara18") == "tamamlandi") {
+  Ara18.checked = true;
+}
+
+Ara18.onclick = function () {
+  if (localStorage.getItem("Ara18") == "tamamlandi") {
+    Ara18.checked = false;
+    localStorage.setItem("Ara18", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara18", "tamamlandi");
+    Ara18.checked = true;
+  }
+};
+var Ara19 = document.getElementById("ara19");
+
+if (localStorage.getItem("Ara19") == "tamamlandi") {
+  Ara19.checked = true;
+}
+
+Ara19.onclick = function () {
+  if (localStorage.getItem("Ara19") == "tamamlandi") {
+    Ara19.checked = false;
+    localStorage.setItem("Ara19", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara19", "tamamlandi");
+    Ara19.checked = true;
+  }
+};
+var Ara20 = document.getElementById("ara20");
+
+if (localStorage.getItem("Ara20") == "tamamlandi") {
+  Ara20.checked = true;
+}
+
+Ara20.onclick = function () {
+  if (localStorage.getItem("Ara20") == "tamamlandi") {
+    Ara20.checked = false;
+    localStorage.setItem("Ara20", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara20", "tamamlandi");
+    Ara20.checked = true;
+  }
+};
+var Ara21 = document.getElementById("ara21");
+
+if (localStorage.getItem("Ara21") == "tamamlandi") {
+  Ara21.checked = true;
+}
+
+Ara21.onclick = function () {
+  if (localStorage.getItem("Ara21") == "tamamlandi") {
+    Ara21.checked = false;
+    localStorage.setItem("Ara21", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara21", "tamamlandi");
+    Ara21.checked = true;
+  }
+};
+var Ara22 = document.getElementById("ara22");
+
+if (localStorage.getItem("Ara22") == "tamamlandi") {
+  Ara22.checked = true;
+}
+
+Ara22.onclick = function () {
+  if (localStorage.getItem("Ara22") == "tamamlandi") {
+    Ara22.checked = false;
+    localStorage.setItem("Ara22", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara22", "tamamlandi");
+    Ara22.checked = true;
+  }
+};
+var Ara23 = document.getElementById("ara23");
+
+if (localStorage.getItem("Ara23") == "tamamlandi") {
+  Ara23.checked = true;
+}
+
+Ara23.onclick = function () {
+  if (localStorage.getItem("Ara23") == "tamamlandi") {
+    Ara23.checked = false;
+    localStorage.setItem("Ara23", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara23", "tamamlandi");
+    Ara23.checked = true;
+  }
+};
+var Ara24 = document.getElementById("ara24");
+
+if (localStorage.getItem("Ara24") == "tamamlandi") {
+  Ara24.checked = true;
+}
+
+Ara24.onclick = function () {
+  if (localStorage.getItem("Ara24") == "tamamlandi") {
+    Ara24.checked = false;
+    localStorage.setItem("Ara24", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara24", "tamamlandi");
+    Ara24.checked = true;
+  }
+};
+var Ara25 = document.getElementById("ara25");
+
+if (localStorage.getItem("Ara25") == "tamamlandi") {
+  Ara25.checked = true;
+}
+
+Ara25.onclick = function () {
+  if (localStorage.getItem("Ara25") == "tamamlandi") {
+    Ara25.checked = false;
+    localStorage.setItem("Ara25", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara25", "tamamlandi");
+    Ara25.checked = true;
+  }
+};
+var Ara26 = document.getElementById("ara26");
+
+if (localStorage.getItem("Ara26") == "tamamlandi") {
+  Ara26.checked = true;
+}
+
+Ara26.onclick = function () {
+  if (localStorage.getItem("Ara26") == "tamamlandi") {
+    Ara26.checked = false;
+    localStorage.setItem("Ara26", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara26", "tamamlandi");
+    Ara26.checked = true;
+  }
+};
+var Ara27 = document.getElementById("ara27");
+
+if (localStorage.getItem("Ara27") == "tamamlandi") {
+  Ara27.checked = true;
+}
+
+Ara27.onclick = function () {
+  if (localStorage.getItem("Ara27") == "tamamlandi") {
+    Ara27.checked = false;
+    localStorage.setItem("Ara27", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara27", "tamamlandi");
+    Ara27.checked = true;
+  }
+};
+var Ara28 = document.getElementById("ara28");
+
+if (localStorage.getItem("Ara28") == "tamamlandi") {
+  Ara28.checked = true;
+}
+
+Ara28.onclick = function () {
+  if (localStorage.getItem("Ara28") == "tamamlandi") {
+    Ara28.checked = false;
+    localStorage.setItem("Ara28", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara28", "tamamlandi");
+    Ara28.checked = true;
+  }
+};
+var Ara29 = document.getElementById("ara29");
+
+if (localStorage.getItem("Ara29") == "tamamlandi") {
+  Ara29.checked = true;
+}
+
+Ara29.onclick = function () {
+  if (localStorage.getItem("Ara29") == "tamamlandi") {
+    Ara29.checked = false;
+    localStorage.setItem("Ara29", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara29", "tamamlandi");
+    Ara29.checked = true;
+  }
+};
+var Ara30 = document.getElementById("ara30");
+
+if (localStorage.getItem("Ara30") == "tamamlandi") {
+  Ara30.checked = true;
+}
+
+Ara30.onclick = function () {
+  if (localStorage.getItem("Ara30") == "tamamlandi") {
+    Ara30.checked = false;
+    localStorage.setItem("Ara30", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara30", "tamamlandi");
+    Ara30.checked = true;
+  }
+};
+var Ara31 = document.getElementById("ara31");
+
+if (localStorage.getItem("Ara31") == "tamamlandi") {
+  Ara31.checked = true;
+}
+
+Ara31.onclick = function () {
+  if (localStorage.getItem("Ara31") == "tamamlandi") {
+    Ara31.checked = false;
+    localStorage.setItem("Ara31", "tamamlanmadi");
+  } else {
+    localStorage.setItem("Ara31", "tamamlandi");
+    Ara31.checked = true;
+  }
+};
